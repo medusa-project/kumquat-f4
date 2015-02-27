d69a4d9b5a9fbd6cde3daa3a0bfb95b84ff3612a
Alex Dolski <alexd@n-ary.net>
2015-02-26 18:12:20 -0600

    Disable log files and make the logger available from initializers

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Make the logger available from initializers as well as disable log files.
+Rails.logger = Logger.new(STDOUT)
+
 # Initialize the Rails application.
 Rails.application.initialize!

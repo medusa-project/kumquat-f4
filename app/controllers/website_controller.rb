5dd0d2f942ceb686e2ad8180749b43e52fd43925
Alex Dolski <alexd@n-ary.net>
2015-03-16 16:40:19 -0500

    Enable local view/style overrides

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,11 +1,22 @@
 class WebsiteController < ApplicationController
 
+  before_filter :prepend_view_paths
+
   def setup
     super
     @num_items = Item.count
     @num_audios = Item.where(Solr::Solr::MEDIA_TYPE_KEY => 'audio/*').count
     @num_images = Item.where(Solr::Solr::MEDIA_TYPE_KEY => 'image/*').count
     @num_videos = Item.where(Solr::Solr::MEDIA_TYPE_KEY => 'video/*').count
+  end
+
+  private
+
+  ##
+  # Allow users to override view templates by adding them to app/local/views.
+  #
+  def prepend_view_paths
+    prepend_view_path('local/views')
   end
 
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,10 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << File.join(Rails.root, 'local', 'assets', 'images')
+Rails.application.config.assets.paths << File.join(Rails.root, 'local', 'assets', 'stylesheets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w(admin.css)
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/

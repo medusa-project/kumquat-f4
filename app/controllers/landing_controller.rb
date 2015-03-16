e4f4cd4343b8bdd520eb40ce462998e3136d85c3
Alex Dolski <alexd@n-ary.net>
2015-03-16 17:27:26 -0500

    Add content to landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,6 +4,9 @@ class LandingController < WebsiteController
   # Responds to GET /
   #
   def index
+    # get a random image item to show
+    @random_item = Item.where(Solr::Solr::MEDIA_TYPE_KEY => 'image/*').
+        facet(false).order("random_#{SecureRandom.hex}").first
   end
 
 end

3b459e2c30de65f5b20bd27cb51d5b2a0743edc9
Alex Dolski <alexd@n-ary.net>
2015-04-02 20:06:46 -0500

    Extract permission-checking into check_permissions

--- app/commands/command_executor.rb
+++ app/commands/command_executor.rb
@@ -12,20 +12,7 @@ class CommandExecutor
   # @raise RuntimeError
   #
   def execute(command)
-    if @doing_user
-      # check the user's permissions
-      missing_permissions = command.required_permissions.reject do |p|
-        @doing_user.can?(p)
-      end
-      if missing_permissions.any?
-        list = missing_permissions.map do |p|
-          perm = Permission.find_by_key(p)
-          return perm ? perm.name.downcase : p
-        end
-        raise SecurityError, "#{@doing_user.username} has insufficient "\
-        "privileges for the following actions: #{list.join(', ')}"
-      end
-    end
+    check_permissions(command) if @doing_user
 
     begin
       command.execute
@@ -45,6 +32,22 @@ class CommandExecutor
     else
       message = "#{command.class.to_s} succeeded"
       Rails.logger.info(message)
+    end
+  end
+
+  private
+
+  def check_permissions(command)
+    missing_permissions = command.required_permissions.reject do |p|
+      @doing_user.can?(p)
+    end
+    if missing_permissions.any?
+      list = missing_permissions.map do |p|
+        perm = Permission.find_by_key(p)
+        return perm ? perm.name.downcase : p
+      end
+      raise SecurityError, "#{@doing_user.username} has insufficient "\
+        "privileges for the following actions: #{list.join(', ')}"
     end
   end
 

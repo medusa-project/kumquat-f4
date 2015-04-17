medusa-project/kumquat-f4

commit 13b346e090039a06939f6a0fa4fb61a303a0b950

Add transactional import

lib/import/importer.rb
@@ -15,82 +15,92 @@ module Import
       item_count = @import_delegate.total_number_of_items.to_i
       return if item_count < 1 # nothing to do
 
-      item_count.times do |index|
-        # retrieve or create the collection
-        key = @import_delegate.collection_key_of_item_at_index(index)
+      ActiveKumquat::Base.transaction do |transaction_url|
+        item_count.times do |index|
+          # retrieve or create the collection
+          key = @import_delegate.collection_key_of_item_at_index(index)
 
-        if @collections[key]
-          collection = @collections[key]
-        else
-          collection = Repository::Collection.find_by_key(key)
-        end
-        unless collection
-          collection = Repository::Collection.new(
-              key: key,
-              container_url: @import_delegate.root_container_url,
-              published: @import_delegate.collection_of_item_at_index_is_published(index),
-              requested_slug: @import_delegate.slug_of_collection_of_item_at_index(index),
-              rdf_graph: @import_delegate.metadata_of_collection_of_item_at_index(index))
-          Rails.logger.debug collection.title if collection.title
-          collection.save!
-          @collections[key] = collection
-        end
-
-        # determine the resource URI under which the item will be created
-        parent_uri = nil
-        parent_import_id = @import_delegate.
-            parent_import_id_of_item_at_index(index)
-        parent_uri ||= @import_id_uri_map[parent_import_id]
-
-        # create the item
-        item = Repository::Item.new(
-            collection: collection,
-            container_url: parent_uri || collection.repository_url,
-            full_text: @import_delegate.full_text_of_item_at_index(index),
-            requested_slug: @import_delegate.slug_of_item_at_index(index),
-            web_id: @import_delegate.web_id_of_item_at_index(index),
-            parent_uri: parent_uri,
-            rdf_graph: @import_delegate.metadata_of_item_at_index(index))
-        item.save! # save it in order to populate its repository URL
-        Rails.logger.debug "Created #{item.repository_url} (#{index + 1}/#{item_count})"
-
-        import_id = @import_delegate.import_id_of_item_at_index(index)
-        @import_id_uri_map[import_id] = item.repository_url
-
-        # append bytestream
-        pathname = @import_delegate.master_pathname_of_item_at_index(index)
-        if pathname
-          if File.exists?(pathname)
-            bs = Repository::Bytestream.new(
-                owner: item,
-                upload_pathname: pathname,
-                type: Repository::Bytestream::Type::MASTER)
-            # assign media type
-            media_type = @import_delegate.media_type_of_item_at_index(index)
-            bs.media_type = media_type unless media_type.blank?
-            bs.save
-            item.bytestreams << bs
+          if @collections[key]
+            collection = @collections[key]
           else
-            Rails.logger.warn "#{pathname} does not exist"
+            collection = Repository::Collection.find_by_key(key)
           end
-        else
-          url = @import_delegate.master_url_of_item_at_index(index)
-          if url
-            bs = Repository::Bytestream.new(
-                owner: item,
-                external_resource_url: url,
-                type: Repository::Bytestream::Type::MASTER)
-            # assign media type
-            media_type = @import_delegate.media_type_of_item_at_index(index)
-            bs.media_type = media_type unless media_type.blank?
-            bs.save
-            item.bytestreams << bs
+          unless collection
+            collection = Repository::Collection.new(
+                key: key,
+                container_url: @import_delegate.root_container_url,
+                published: @import_delegate.collection_of_item_at_index_is_published(index),
+                requested_slug: @import_delegate.slug_of_collection_of_item_at_index(index),
+                rdf_graph: @import_delegate.metadata_of_collection_of_item_at_index(index),
+                transaction_url: transaction_url)
+            Rails.logger.debug collection.title if collection.title
+            collection.save!
+            @collections[key] = collection
           end
-        end
+          collection.transaction_url = transaction_url
 
-        item.generate_derivatives
-        item.save!
+          # determine the resource URI under which the item will be created
+          parent_import_id = @import_delegate.
+              parent_import_id_of_item_at_index(index)
+          parent_uri = @import_id_uri_map[parent_import_id]
+
+          # create the item
+          item = Repository::Item.new(
+              collection: collection,
+              container_url: parent_uri || collection.repository_url,
+              full_text: @import_delegate.full_text_of_item_at_index(index),
+              requested_slug: @import_delegate.slug_of_item_at_index(index),
+              web_id: @import_delegate.web_id_of_item_at_index(index),
+              parent_uri: parent_uri,
+              rdf_graph: @import_delegate.metadata_of_item_at_index(index),
+              transaction_url: transaction_url)
+          item.save! # save it in order to populate its repository URL
+          Rails.logger.debug "Created #{item.repository_url} (#{index + 1}/#{item_count})"
+
+          import_id = @import_delegate.import_id_of_item_at_index(index)
+          @import_id_uri_map[import_id] = item.repository_url
+
+          # append bytestream
+          pathname = @import_delegate.master_pathname_of_item_at_index(index)
+          if pathname
+            if File.exists?(pathname)
+              bs = Repository::Bytestream.new(
+                  owner: item,
+                  upload_pathname: pathname,
+                  type: Repository::Bytestream::Type::MASTER,
+                  transaction_url: transaction_url)
+              # assign media type
+              media_type = @import_delegate.media_type_of_item_at_index(index)
+              bs.media_type = media_type unless media_type.blank?
+              bs.save
+              item.bytestreams << bs
+            else
+              Rails.logger.warn "#{pathname} does not exist"
+            end
+          else
+            url = @import_delegate.master_url_of_item_at_index(index)
+            if url
+              bs = Repository::Bytestream.new(
+                  owner: item,
+                  external_resource_url: url,
+                  type: Repository::Bytestream::Type::MASTER,
+                  transaction_url: transaction_url)
+              # assign media type
+              media_type = @import_delegate.media_type_of_item_at_index(index)
+              bs.media_type = media_type unless media_type.blank?
+              bs.save
+              item.bytestreams << bs
+            end
+          end
+
+          item.generate_derivatives
+          item.save!
+        end
       end
+      solr_url = Kumquat::Application.kumquat_config[:solr_url].chomp('/')
+      solr_collection = Kumquat::Application.kumquat_config[:solr_collection]
+      puts "Import complete. Remember to commit the Solr index once it has "\
+      "ingested everything: #{solr_url}/#{solr_collection}/update?commit=true"
     end
 
   end

app/models/concerns/derivative_management.rb
@@ -62,7 +62,8 @@ module DerivativeManagement
             owner: item,
             upload_pathname: tempfile.path + '.jpg',
             media_type: 'image/jpeg',
-            type: Repository::Bytestream::Type::DERIVATIVE)
+            type: Repository::Bytestream::Type::DERIVATIVE,
+            transaction_url: self.transaction_url)
         bs.save
         item.bytestreams << bs
       end
@@ -84,7 +85,8 @@ module DerivativeManagement
               owner: item,
               upload_pathname: tempfile.path + '.jpg',
               media_type: 'image/jpeg',
-              type: Repository::Bytestream::Type::DERIVATIVE)
+              type: Repository::Bytestream::Type::DERIVATIVE,
+              transaction_url: self.transaction_url)
           bs.save
           item.bytestreams << bs
         end

app/models/repository/bytestream.rb
@@ -8,6 +8,7 @@ module Repository
   class Bytestream
 
     include ActiveModel::Model
+    include ActiveKumquat::Transactions
 
     ENTITY_CLASS = ActiveKumquat::Base::Class::BYTESTREAM
 
@@ -17,6 +18,7 @@ module Repository
     attr_accessor :media_type # string
     attr_accessor :owner # ActiveKumquat::Base subclass
     attr_accessor :repository_url # string
+    attr_accessor :transaction_url # string
     attr_accessor :type # Bytestream::Type
     attr_accessor :upload_pathname # string
     attr_accessor :uuid # string
@@ -62,7 +64,7 @@ module Repository
     #
     def delete(also_tombstone = false, commit_immediately = true)
       if self.repository_url
-        url = self.repository_url.chomp('/')
+        url = transactionalized_url(self.repository_url).chomp('/')
         @@http.delete(url)
         @@http.delete("#{url}/fcr:tombstone") if also_tombstone
         @destroyed = true
@@ -176,15 +178,16 @@ module Repository
     end
 
     def reload!
-      response = @@http.get(self.repository_metadata_url, nil,
-                            { 'Accept' => 'application/n-triples' })
+      url = self.repository_metadata_url # already transactionalized
+      response = @@http.get(url, nil, { 'Accept' => 'application/n-triples' })
       graph = RDF::Graph.new
       graph.from_ntriples(response.body)
       self.populate_from_graph(graph)
     end
 
     def repository_metadata_url
-      "#{self.repository_url.chomp('/')}/fcr:metadata"
+      url = transactionalized_url(self.repository_url).chomp('/')
+      "#{url}/fcr:metadata"
     end
 
     def save(commit_immediately = true) # TODO: look into Solr soft commits
@@ -274,8 +277,9 @@ module Repository
     # Updates an existing bytestream.
     #
     def save_existing
+      url = self.repository_metadata_url # already transactionalized
       update = self.to_sparql_update
-      @@http.patch(self.repository_metadata_url, update.to_s,
+      @@http.patch(url, update.to_s,
                    { 'Content-Type' => 'application/sparql-update' })
     end
 
@@ -294,16 +298,19 @@ module Repository
               'Content-Disposition' => "attachment; filename=\"#{filename}\""
           }
           headers['Content-Type'] = self.media_type unless self.media_type.blank?
-          response = @@http.post(self.owner.repository_url, file, headers)
+          url = transactionalized_url(self.owner.repository_url)
+          response = @@http.post(url, file, headers)
         end
       elsif self.external_resource_url
-        response = @@http.post(self.owner.repository_url, nil,
+        url = transactionalized_url(self.owner.repository_url)
+        response = @@http.post(url, nil,
                                { 'Content-Type' => 'text/plain' })
         headers = { 'Content-Type' => "message/external-body; "\
           "access-type=URL; URL=\"#{self.external_resource_url}\"" }
         @@http.put(response.header['Location'].first, nil, headers)
       end
-      self.repository_url = response.header['Location'].first
+      self.repository_url = detransactionalized_url(
+          response.header['Location'].first)
       save_existing
     end
 

lib/active_kumquat/base.rb
@@ -11,6 +11,7 @@ module ActiveKumquat
     extend ActiveModel::Callbacks
     include ActiveModel::Model
     include Describable
+    include Transactions
 
     define_model_callbacks :create, :delete, :load, :save, :update,
                            only: [:after, :before]
@@ -26,13 +27,37 @@ module ActiveKumquat
     attr_reader :bytestreams # Set of Bytestreams
     attr_accessor :container_url # URL of the entity's parent container
     attr_accessor :rdf_graph
-    attr_accessor :repository_url
+    attr_accessor :repository_url # the entity's repository URL outside of any transaction
     attr_accessor :requested_slug # requested F4 last path component for new entities
     attr_accessor :solr_json
+    attr_accessor :transaction_url
     attr_accessor :uuid
     alias_method :id, :uuid
 
     validates :uuid, allow_nil: true, length: { minimum: 36, maximum: 36 }
+
+    ##
+    # Executes a block within a transaction. Use like:
+    #
+    # ActiveKumquat::Base.transaction do |transaction_url|
+    #   (code to run within the transaction)
+    # end
+    #
+    # See https://wiki.duraspace.org/display/FEDORA41/Transactions
+    #
+    # @param url Transaction base URL
+    #
+    def self.transaction
+      url = create_transaction(@@http)
+      begin
+        yield url
+      rescue => e
+        rollback_transaction(url, @@http)
+        raise e
+      else
+        commit_transaction(url, @@http)
+      end
+    end
 
     def initialize(params = {})
       @bytestreams = Set.new
@@ -49,9 +74,10 @@ module ActiveKumquat
     # @param commit_immediately boolean
     #
     def delete(also_tombstone = false, commit_immediately = true)
-      if self.repository_url
+      url = transactionalized_url(self.repository_url)
+      if url
         run_callbacks :delete do
-          url = self.repository_url.chomp('/')
+          url = url.chomp('/')
           @@http.delete(url)
           @@http.delete("#{url}/fcr:tombstone") if also_tombstone
           @destroyed = true
@@ -101,7 +127,8 @@ module ActiveKumquat
           self.uuid = statement.object.to_s
         elsif statement.predicate.to_s == kq_uri + kq_predicates::BYTESTREAM_URI
           bs = Repository::Bytestream.new(owner: self,
-                                          repository_url: statement.object.to_s)
+                                          repository_url: statement.object.to_s,
+                                          transaction_url: self.transaction_url)
           bs.reload!
           self.bytestreams << bs
         end
@@ -111,7 +138,8 @@ module ActiveKumquat
     end
 
     def reload!
-      response = @@http.get(self.repository_url, nil,
+      url = transactionalized_url(self.repository_url)
+      response = @@http.get(url, nil,
                             { 'Accept' => 'application/n-triples' })
       graph = RDF::Graph.new
       graph.from_ntriples(response.body)
@@ -202,11 +230,14 @@ module ActiveKumquat
       self.save!
     end
 
+    protected
+
     ##
     # Updates an existing item.
     #
     def save_existing
-      @@http.patch(self.repository_url, self.to_sparql_update.to_s,
+      url = transactionalized_url(self.repository_url)
+      @@http.patch(url, self.to_sparql_update.to_s,
                    { 'Content-Type' => 'application/sparql-update' })
     end
 
@@ -215,14 +246,15 @@ module ActiveKumquat
     #
     def save_new
       run_callbacks :create do
+        url = transactionalized_url(self.container_url)
         # As of version 4.1, Fedora doesn't like to accept triples via POST for
         # some reason; it just returns 201 Created regardless of the Content-Type
         # header and body content. So we will POST to create an empty container,
         # and then update that.
         headers = { 'Content-Type' => 'application/n-triples' }
         headers['slug'] = self.requested_slug if self.requested_slug
-        response = @@http.post(self.container_url, nil, headers)
-        self.repository_url = response.header['Location'].first
+        response = @@http.post(url, nil, headers)
+        self.repository_url = detransactionalized_url(response.header['Location'].first)
         self.requested_slug = nil
         save_existing
       end

lib/active_kumquat/transactions.rb
@@ -1,0 +1,88 @@
+module ActiveKumquat
+
+  module Transactions
+
+    def self.included(mod)
+      mod.extend ClassMethods
+    end
+
+    module ClassMethods
+
+      ##
+      # Creates a transaction.
+      # https://wiki.duraspace.org/display/FEDORA41/Transactions
+      #
+      # @returns string Transaction base URL
+      #
+      def create_transaction(http_client)
+        repository_url = Kumquat::Application.kumquat_config[:fedora_url].
+            chomp('/')
+        url = repository_url + '/fcr:tx'
+        response = http_client.post(url)
+        response.header['Location'].first
+      end
+
+      ##
+      # Commits the transaction with the given ID.
+      # https://wiki.duraspace.org/display/FEDORA41/Transactions
+      #
+      # @param id string
+      # @param http_client HTTPClient
+      # @return HTTPResponse
+      #
+      def commit_transaction(id, http_client)
+        http_client.post(id + '/fcr:tx/fcr:commit')
+      end
+
+      ##
+      # Rolls back the transaction with the given ID.
+      #
+      # @param id string
+      # @param http_client HTTPClient
+      # @return HTTPResponse
+      #
+      def rollback_transaction(id, http_client)
+        http_client.post(id + '/fcr:tx/fcr:rollback')
+      end
+
+    end
+
+    ##
+    # Converts the given URL into a non-transactional URL based on the current
+    # value of transaction_url. If transaction_url is nil, returns the given
+    # URL unchanged.
+    #
+    # See https://wiki.duraspace.org/display/FEDORA41/Transactions
+    #
+    # @param url string
+    # @return string
+    #
+    def detransactionalized_url(url)
+      if self.transaction_url
+        return url.gsub(self.transaction_url.chomp('/'),
+                        Kumquat::Application.kumquat_config[:fedora_url].chomp('/'))
+      end
+      url
+    end
+
+    ##
+    # Converts the given URL into a transactional URL based on the current
+    # value of transaction_url. If transaction_url is nil, returns the given
+    # URL unchanged.
+    #
+    # See https://wiki.duraspace.org/display/FEDORA41/Transactions
+    #
+    # @param url string
+    # @return string
+    #
+    def transactionalized_url(url)
+      f4_url = Kumquat::Application.kumquat_config[:fedora_url].chomp('/')
+      if self.transaction_url and !url.start_with?(f4_url + '/tx:')
+        return url.gsub(f4_url, self.transaction_url.chomp('/'))
+      end
+      url
+    end
+
+  end
+
+end

test/lib/active_kumquat/transactions_test.rb
@@ -1,0 +1,29 @@
+require 'test_helper'
+
+class TransactionsTest < ActiveSupport::TestCase
+
+  class Foo
+    include ActiveKumquat::Transactions
+    attr_accessor :transaction_url
+  end
+
+  setup do
+    @f4_url = Kumquat::Application.kumquat_config[:fedora_url].chomp('/')
+    @obj = Foo.new
+    @obj.transaction_url = @f4_url + '/tx:23942034982340'
+    @non_tx_url = @f4_url + '/bla/bla/bla'
+    @tx_url = @obj.transaction_url + '/bla/bla/bla'
+  end
+
+  test 'detransactionalized_url should work properly' do
+    assert_equal @non_tx_url, @obj.detransactionalized_url(@tx_url)
+  end
+
+  test 'transactionalized_url should work properly' do
+    assert_equal @tx_url, @obj.transactionalized_url(@non_tx_url)
+
+    # don't transactionalize an already-transactional url
+    assert_equal @tx_url, @obj.transactionalized_url(@tx_url)
+  end
+
+end

commit 9288c806c276bd9d800c0e5573ea9ad5bfbc9af4

Finder methods support transactions (fixes a regression introduced in 13b346e)

app/models/repository/collection.rb
@@ -18,21 +18,25 @@ module Repository
     # Convenience method that deletes a collection with the given key.
     #
     # @param key
+    # @param transaction_url string
     #
-    def self.delete_with_key(key)
+    def self.delete_with_key(key, transaction_url = nil)
       root_container_url = Kumquat::Application.kumquat_config[:fedora_url]
       col_to_delete = Repository::Collection.new(
           repository_url: "#{root_container_url.chomp('/')}/#{key}",
-          key: key)
+          key: key,
+          transaction_url: transaction_url)
       col_to_delete.delete(true)
     end
 
     ##
     # @param key string
+    # @param transaction_url string
     # @return Entity
     #
-    def self.find_by_key(key)
-      self.where(Solr::Solr::COLLECTION_KEY_KEY => key).first
+    def self.find_by_key(key, transaction_url = nil)
+      self.where(Solr::Solr::COLLECTION_KEY_KEY => key).
+          use_transaction_url(transaction_url).first rescue nil
     end
 
     ##

lib/active_kumquat/entity.rb
@@ -5,9 +5,12 @@ module ActiveKumquat
   #
   class Entity
 
+    include Transactions
+
     @@http = HTTPClient.new
 
     attr_reader :solr_request
+    attr_accessor :transaction_url
 
     ##
     # @param caller The calling entity (ActiveKumquat::Entity subclass), or
@@ -101,6 +104,11 @@ module ActiveKumquat
       self
     end
 
+    def use_transaction_url(url)
+      self.transaction_url = url
+      self
+    end
+
     ##
     # @param where Hash or string
     # @return ActiveKumquat::Entity
@@ -152,9 +160,10 @@ module ActiveKumquat
               solr_response['facet_counts']['facet_fields']) if @facet
           solr_response['response']['docs'].each do |doc|
             entity = @caller.new(solr_json: doc, repository_url: doc['id'])
-
-            f4_response = @@http.get(doc['id'], nil,
-                                  { 'Accept' => 'application/n-triples' })
+            url = doc['id']
+            url = transactional_url(url) if self.transaction_url.present?
+            f4_response = @@http.get(url, nil,
+                                     { 'Accept' => 'application/n-triples' })
             graph = RDF::Graph.new
             graph.from_ntriples(f4_response.body)
             entity.populate_from_graph(graph)

lib/active_kumquat/querying.rb
@@ -11,34 +11,39 @@ module ActiveKumquat
 
     ##
     # @param id UUID
+    # @param transaction_url string
     # @return Entity
     #
-    def find(id)
-      self.find_by_uuid(id)
+    def find(id, transaction_url = nil)
+      self.find_by_uuid(id, transaction_url)
     end
 
     ##
     # @param uri Fedora resource URI
+    # @param transaction_url string
     # @return Entity
     #
-    def find_by_uri(uri)
-      self.where(id: "\"#{uri}\"").first
+    def find_by_uri(uri, transaction_url = nil)
+      self.where(id: "\"#{uri}\"").use_transaction_url(transaction_url).first
     end
 
     ##
     # @param uuid string
+    # @param transaction_url string
     # @return Entity
     #
-    def find_by_uuid(uuid)
-      self.where(uuid: uuid).first
+    def find_by_uuid(uuid, transaction_url = nil)
+      self.where(uuid: uuid).use_transaction_url(transaction_url).first
     end
 
     ##
     # @param web_id string
+    # @param transaction_url string
     # @return Entity
     #
-    def find_by_web_id(web_id)
-      self.where(Solr::Solr::WEB_ID_KEY => web_id).first
+    def find_by_web_id(web_id, transaction_url = nil)
+      self.where(Solr::Solr::WEB_ID_KEY => web_id).
+          use_transaction_url(transaction_url).first
     end
 
     def method_missing(name, *args, &block)

lib/import/importer.rb
@@ -23,7 +23,8 @@ module Import
           if @collections[key]
             collection = @collections[key]
           else
-            collection = Repository::Collection.find_by_key(key)
+            collection = Repository::Collection.find_by_key(key,
+                                                            transaction_url)
           end
           unless collection
             collection = Repository::Collection.new(
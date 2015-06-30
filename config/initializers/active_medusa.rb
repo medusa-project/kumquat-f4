9945e74ddab01587ef35213953cbd17cfe4156d8
Alex Dolski <alexd@n-ary.net>
2015-06-30 16:14:18 -0500

    Fix activemedusa initialization

--- config/initializers/active_medusa.rb
+++ config/initializers/active_medusa.rb
@@ -6,13 +6,9 @@ ActiveMedusa::Configuration.new do |config|
   config.solr_url = 'http://localhost:8983/solr'
   config.solr_core = 'kumquat'
   config.solr_more_like_this_endpoint = '/mlt'
-  config.solr_class_field = :class_s
+  config.solr_class_field = Solr::Fields::CLASS
   config.solr_uri_field = :id
-  config.solr_uuid_field = :uuid_s
-  config.solr_default_search_field = :searchall_txt
-  config.solr_facet_fields = [
-      :collection_facet, :contributor_facet, :coverage_facet, :creator_facet,
-      :date_facet, :format_facet, :language_facet, :publisher_facet,
-      :source_facet, :subject_facet, :type_facet
-  ]
+  config.solr_uuid_field = Solr::Fields::UUID
+  config.solr_default_search_field = Solr::Fields::SEARCH_ALL
+  config.solr_facet_fields = Solr::Fields::FACET_FIELDS
 end

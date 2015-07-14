14927c22c6743da93203951c7d9a1f39a6ff4f6c
Alex Dolski <alexd@n-ary.net>
2015-07-14 16:33:29 -0500

    Use values from kumquat configuration

--- config/initializers/active_medusa.rb
+++ config/initializers/active_medusa.rb
@@ -1,9 +1,11 @@
+require_relative 'kumquat'
+
 ActiveMedusa::Configuration.new do |config|
-  config.fedora_url = 'http://localhost:8080/fedora/rest'
+  config.fedora_url = Kumquat::Application.kumquat_config[:fedora_url]
   config.logger = Rails.logger
   config.class_predicate = 'http://example.org/hasClass'
-  config.solr_url = 'http://localhost:8983/solr'
-  config.solr_core = 'kumquat'
+  config.solr_url = Kumquat::Application.kumquat_config[:solr_url]
+  config.solr_core = Kumquat::Application.kumquat_config[:solr_core]
   config.solr_more_like_this_endpoint = '/mlt'
   config.solr_class_field = Solr::Fields::CLASS
   config.solr_uri_field = :id

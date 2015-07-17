db085f6b8409e563cf874fa3ea4cbd4c406959e0
Alex Dolski <alexd@n-ary.net>
2015-07-17 09:24:03 -0500

    Fix Turtle export and temporarily disable RDF/XML and JSON-LD

--- app/controllers/concerns/rdf_streamer.rb
+++ app/controllers/concerns/rdf_streamer.rb
@@ -2,24 +2,44 @@ class RDFStreamer
 
   ##
   # @param describables [ActiveMedusa::Relation<Describable>]
-  # @param format One of :jsonld, :ttl, or :rdf
+  # @param format [Symbol] One of :jsonld, :ttl, or :rdf
   def initialize(describables, format)
     @describables = describables
     @format = format
   end
 
   def each
-    @describables.each do |d|
-      case @format
-        when :jsonld
-          yield d.rdf_graph.to_jsonld
-        when :rdf
-          yield d.rdf_graph.to_rdfxml
-        when :ttl
-          yield d.rdf_graph.to_ttl
-      end
-
+    case @format
+      when :jsonld # TODO: this is broken
+        yield 'opener'
+        @describables.each { |d| yield d.rdf_graph.to_jsonld }
+        yield 'bla'
+      when :rdf # TODO: this is broken
+        yield 'opener'
+        @describables.each { |d| yield d.rdf_graph.to_rdfxml }
+        yield 'bla'
+      when :ttl
+        @describables.each { |d| yield to_ttl_fragment(d.rdf_graph) }
     end
+  end
+
+  private
+
+  def to_ttl_fragment(graph)
+    ttl = ''
+    graph.each_statement do |st|
+      subject = "<#{st.subject.to_s}>"
+      predicate = "<#{st.predicate.to_s}>"
+      if st.object.kind_of?(RDF::URI)
+        object = "<#{st.object.to_s}>"
+      elsif st.object.to_s.lines.count > 1
+        object = "\"\"\"#{st.object.to_s}\"\"\""
+      else
+        object = "\"#{st.object.to_s}\""
+      end
+      ttl += "#{subject} #{predicate} #{object} .\n"
+    end
+    ttl
   end
 
 end

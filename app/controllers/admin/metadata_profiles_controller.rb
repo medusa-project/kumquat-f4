14a491a3bb1cba051e83bd7cb15aac6959dca380
Alex Dolski <alexd@n-ary.net>
2015-07-21 14:55:44 -0500

    Can create metadata profiles

--- app/controllers/admin/metadata_profiles_controller.rb
+++ app/controllers/admin/metadata_profiles_controller.rb
@@ -2,6 +2,27 @@ module Admin
 
   class MetadataProfilesController < ControlPanelController
 
+    def create
+      @profile = MetadataProfile.new(sanitized_params)
+      begin
+        @profile.save!
+      rescue ActiveRecord::RecordInvalid
+        response.headers['X-Kumquat-Result'] = 'error'
+        render partial: 'shared/validation_messages',
+               locals: { entity: @profile }
+      rescue => e
+        response.headers['X-Kumquat-Result'] = 'error'
+        flash['error'] = "#{e}"
+        keep_flash
+        render 'create'
+      else
+        response.headers['X-Psap-Result'] = 'success'
+        flash['success'] = "Metadata profile \"#{@profile.name}\" created."
+        keep_flash
+        render 'create' # create.js.erb will reload the page
+      end
+    end
+
     def index
       @profiles = MetadataProfile.order(:name)
       @new_profile = MetadataProfile.new

542402faa6f3d0b14ab257054033833db627c278
Alex Dolski <alexd@n-ary.net>
2015-07-23 16:35:47 -0500

    Validate name and solr_field

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -2,7 +2,10 @@ class Facet < ActiveRecord::Base
 
   has_many :triples, inverse_of: :facet, dependent: :nullify
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2 },
+            uniqueness: { case_sensitive: false }
+  validates :solr_field, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :solr_field, with: /\w*_facet\b/ # must end with "_facet"
 
   def to_s
     self.name

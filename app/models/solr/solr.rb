38adda998b9ee5516b53f46f89416067cc6e1f0f
Alex Dolski <alexd@n-ary.net>
2015-08-21 11:08:04 -0500

    Move Solr::Solr::SCHEMA to schema.yml and modify Solr::Solr.update_schema to delete obsolete fields; also eliminate schema.xml as the schema is now fully dynamic

--- app/models/solr/solr.rb
+++ app/models/solr/solr.rb
@@ -2,325 +2,7 @@ module Solr
 
   class Solr
 
-    ##
-    # Schema summary:
-    #
-    # uri_*_txt          arbitrary RDF objects
-    # kq_meta_*          normalized metadata schema into which many of the
-    #                    uri_* fields are copied (notably, /dc/elements/1.1 and
-    #                    /dc/terms are merged)
-    # kq_meta_title_s    Single-valued title field that can be sorted (must be
-    #                    populated manually)
-    # kq_meta_date_dts   Single-valued date field
-    # kq_sys_*           system properties
-    # kq_*_facet         facets
-    # kq_searchall_txt   full-text search field
-    #
-    # There are no classic fields, only dynamicFields and copyFields.
-    #
-    # The values of this hash will be POSTed as-is to Solr's schema API
-    # endpoint.
-    #
-    SCHEMA = {
-        copyFields: [ # TODO: replace these with a "normalized fields" feature
-            # Map various fields to Kumquat's own schema. The main purpose
-            # of this is to normalize the DC elements and terms.
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_contributor_txt',
-                dest: 'kq_meta_contributor_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_coverage_txt',
-                dest: 'kq_meta_coverage_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_creator_txt',
-                dest: 'kq_meta_creator_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_date_txt',
-                dest: 'kq_meta_date_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_description_txt',
-                dest: 'kq_meta_description_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_format_txt',
-                dest: 'kq_meta_format_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_identifier_s',
-                dest: 'kq_meta_identifier_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_language_txt',
-                dest: 'kq_meta_language_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_publisher_txt',
-                dest: 'kq_meta_publisher_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_relation_txt',
-                dest: 'kq_meta_relation_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_rights_txt',
-                dest: 'kq_meta_rights_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_source_txt',
-                dest: 'kq_meta_source_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_subject_txt',
-                dest: 'kq_meta_subject_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_title_txt',
-                dest: 'kq_meta_title_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_elements_1_1_type_txt',
-                dest: 'kq_meta_type_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_abstract_txt',
-                dest: 'kq_meta_abstract_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_accessRights_txt',
-                dest: 'kq_meta_accessRights_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_accrualMethod_txt',
-                dest: 'kq_meta_accrualMethod_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_accrualPeriodicity_txt',
-                dest: 'kq_meta_accrualPeriodicity_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_accrualPolicy_txt',
-                dest: 'kq_meta_accrualPolicy_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_alternative_txt',
-                dest: 'kq_meta_alternative_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_audience_txt',
-                dest: 'kq_meta_audience_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_available_txt',
-                dest: 'kq_meta_available_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_bibliographicCitation_txt',
-                dest: 'kq_meta_bibliographicCitation_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_conformsTo_txt',
-                dest: 'kq_meta_conformsTo_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_contributor_txt',
-                dest: 'kq_meta_contributor_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_coverage_txt',
-                dest: 'kq_meta_coverage_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_created_txt',
-                dest: 'kq_meta_created_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_creator_txt',
-                dest: 'kq_meta_creator_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_date_txt',
-                dest: 'kq_meta_date_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_dateAccepted_txt',
-                dest: 'kq_meta_dateAccepted_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_dateCopyrighted_txt',
-                dest: 'kq_meta_dateCopyrighted_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_dateSubmitted_txt',
-                dest: 'kq_meta_dateSubmitted_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_description_txt',
-                dest: 'kq_meta_description_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_educationLevel_txt',
-                dest: 'kq_meta_educationLevel_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_extent_txt',
-                dest: 'kq_meta_extent_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_format_txt',
-                dest: 'kq_meta_format_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_hasFormat_txt',
-                dest: 'kq_meta_hasFormat_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_hasPart_txt',
-                dest: 'kq_meta_hasPart_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_hasVersion_txt',
-                dest: 'kq_meta_hasVersion_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_identifier_txt',
-                dest: 'kq_meta_identifier_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_instructionalMethod_txt',
-                dest: 'kq_meta_instructionalMethod_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isFormatOf_txt',
-                dest: 'kq_meta_isFormatOf_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isPartOf_txt',
-                dest: 'kq_meta_isPartOf_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isReferencedBy_txt',
-                dest: 'kq_meta_isReferencedBy_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isReplacedBy_txt',
-                dest: 'kq_meta_isReplacedBy_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isRequiredBy_txt',
-                dest: 'kq_meta_isRequiredBy_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_issued_txt',
-                dest: 'kq_meta_issued_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_isVersionOf_txt',
-                dest: 'kq_meta_language_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_language_txt',
-                dest: 'kq_meta_language_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_license_txt',
-                dest: 'kq_meta_license_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_mediator_txt',
-                dest: 'kq_meta_mediator_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_MediaType_txt',
-                dest: 'kq_sys_media_type_s'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_medium_txt',
-                dest: 'kq_meta_medium_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_modified_txt',
-                dest: 'kq_meta_modified_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_provenance_txt',
-                dest: 'kq_meta_provenance_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_publisher_txt',
-                dest: 'kq_meta_publisher_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_references_txt',
-                dest: 'kq_meta_references_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_relation_txt',
-                dest: 'kq_meta_relation_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_replaces_txt',
-                dest: 'kq_meta_replaces_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_requires_txt',
-                dest: 'kq_meta_requires_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_rights_txt',
-                dest: 'kq_meta_rights_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_rightsHolder_txt',
-                dest: 'kq_meta_rightsHolder_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_source_txt',
-                dest: 'kq_meta_source_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_spatial_txt',
-                dest: 'kq_meta_spatial_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_subject_txt',
-                dest: 'kq_meta_subject_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_tableOfContents_txt',
-                dest: 'kq_meta_tableOfContents_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_temporal_txt',
-                dest: 'kq_meta_temporal_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_title_txt',
-                dest: 'kq_meta_title_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_type_txt',
-                dest: 'kq_meta_type_txt'
-            },
-            {
-                source: 'uri_http_purl_org_dc_terms_valid_txt',
-                dest: 'kq_meta_valid_txt'
-            }
-        ],
-        dynamicFields: [
-            {
-                name: '*_facet',
-                type: 'string',
-                stored: true,
-                indexed: true,
-                multiValued: true,
-                docValues: true
-            }
-        ]
-    }
+    SCHEMA = YAML.load(File.read(File.join(__dir__, 'schema.yml')))
 
     @@client = RSolr.connect(url: Kumquat::Application.kumquat_config[:solr_url].chomp('/') +
                                  '/' + Kumquat::Application.kumquat_config[:solr_core])
@@ -350,7 +32,6 @@ module Solr
     end
 
     def clear
-      @http = HTTPClient.new
       @http.get(@url + '/update?stream.body=<delete><query>*:*</query></delete>')
       @http.get(@url + '/update?stream.body=<commit/>')
     end
@@ -367,20 +48,24 @@ module Solr
 
     ##
     # Creates the set of fields needed by the application. This requires
-    # Solr 5.2+ with the ManagedIndexSchemaFactory enabled.
-    #
-    # @return [HTTP::Message, nil] Nil if there were no fields to create.
+    # Solr 5.2+ with the ManagedIndexSchemaFactory enabled and a mutable schema.
     #
     def update_schema
-      # Solr will throw an error if we try to add a field that already exists,
-      # so we have to send it only fields that don't already exist.
       response = @http.get("#{@url}/schema")
       current = JSON.parse(response.body)
 
-      # dynamic fields
-      dynamic_fields_to_add = SCHEMA[:dynamicFields].reject do |kf|
+      # delete obsolete dynamic fields
+      dynamic_fields_to_delete = current['schema']['dynamicFields'].select do |cf|
+        !SCHEMA['dynamicFields'].map{ |sf| sf['name'] }.include?(cf['name'])
+      end
+      dynamic_fields_to_delete.each do |df|
+        post_fields('delete-dynamic-field', { 'name' => df['name'] })
+      end
+
+      # add new dynamic fields
+      dynamic_fields_to_add = SCHEMA['dynamicFields'].reject do |kf|
         current['schema']['dynamicFields'].
-            map{ |sf| sf['name'] }.include?(kf[:name])
+            map{ |sf| sf['name'] }.include?(kf['name'])
       end
       post_fields('add-dynamic-field', dynamic_fields_to_add)
 
@@ -396,7 +81,7 @@ module Solr
       facetable_fields_to_add = facetable_fields.reject do |ff|
         current['schema']['copyFields'].
             map{ |sf| "#{sf['source']}-#{sf['dest']}" }.
-            include?("#{ff[:source]}-#{ff[:dest]}")
+            include?("#{ff['source']}-#{ff['dest']}")
       end
       post_fields('add-copy-field', facetable_fields_to_add)
 
@@ -404,15 +89,22 @@ module Solr
       search_all_fields_to_add = search_all_fields.reject do |ff|
         current['schema']['copyFields'].
             map{ |sf| "#{sf['source']}-#{sf['dest']}" }.
-            include?("#{ff[:source]}-#{ff[:dest]}")
+            include?("#{ff['source']}-#{ff['dest']}")
       end
       post_fields('add-copy-field', search_all_fields_to_add)
 
-      # other copyFields
-      copy_fields_to_add = SCHEMA[:copyFields].reject do |kf|
+      # delete obsolete copyFields
+      copy_fields_to_delete = current['schema']['copyFields'].select do |kf|
+        !SCHEMA['copyFields'].map{ |sf| "#{sf['source']}#{sf['dest']}" }.
+            include?("#{kf['source']}#{kf['dest']}")
+      end
+      post_fields('delete-copy-field', copy_fields_to_delete)
+
+      # add new copyFields
+      copy_fields_to_add = SCHEMA['copyFields'].reject do |kf|
         current['schema']['copyFields'].
-            map{ |sf| "#{sf['source']}-#{sf['dest']}" }.
-            include?("#{kf[:source]}-#{kf[:dest]}")
+            map{ |sf| "#{sf['source']}#{sf['dest']}" }.
+            include?("#{kf['source']}#{kf['dest']}")
       end
       post_fields('add-copy-field', copy_fields_to_add)
     end
